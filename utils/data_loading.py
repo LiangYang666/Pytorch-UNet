b46c2f5ba330a9d34485a77f5b4b899a86561507
LiangYang666 <mail.liangyang@gmail.com>
2021-11-10 21:56:08 +0800

    添加DLRSD数据集的datasets加载类

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -1,3 +1,4 @@
+import glob
 import logging
 from os import listdir
 from os.path import splitext
@@ -78,3 +79,83 @@ class BasicDataset(Dataset):
 class CarvanaDataset(BasicDataset):
     def __init__(self, images_dir, masks_dir, scale=1):
         super().__init__(images_dir, masks_dir, scale, mask_suffix='_mask')
+
+
+class DLRSDDatasets(Dataset):
+    def __init__(self, images_dir: str, masks_dir: str, scale: float = 1.0, mask_suffix: str = ''):
+        self.images_dir = Path(images_dir)
+        self.masks_dir = Path(masks_dir)
+        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
+        self.scale = scale
+        self.mask_suffix = mask_suffix
+        # self.ids = glob.glob(images_dir+'/**/*.tif')
+        self.images = glob.glob(images_dir+'/**/*.tif')
+        self.masks = [x.replace('/Images/', '/masks/', 1).replace('.tif', '.npy', 1) for x in self.images]
+        # self.ids = [splitext(file)[0] for file in listdir(images_dir) if not file.startswith('.')]
+        # if not self.ids:
+        #     raise RuntimeError(f'No input file found in {images_dir}, make sure you put your images there')
+        # logging.info(f'Creating dataset with {len(self.ids)} examples')
+        logging.info(f'Creating dataset with {len(self.images)} examples')
+
+    def __len__(self):
+        return len(self.images)
+
+    @classmethod
+    def preprocess(cls, pil_img, scale, is_mask):
+        w, h = pil_img.size
+        newW, newH = int(scale * w), int(scale * h)
+        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
+        pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
+        img_ndarray = np.asarray(pil_img)
+
+        if img_ndarray.ndim == 2 and not is_mask:
+            img_ndarray = img_ndarray[np.newaxis, ...]
+        elif not is_mask:
+            img_ndarray = img_ndarray.transpose((2, 0, 1))
+
+        if not is_mask:
+            img_ndarray = img_ndarray / 255
+
+        return img_ndarray
+
+    @classmethod
+    def load(cls, filename):
+        ext = splitext(filename)[1]
+        if ext in ['.npz', '.npy']:
+            return Image.fromarray(np.load(filename))
+        elif ext in ['.pt', '.pth']:
+            return Image.fromarray(torch.load(filename).numpy())
+        else:
+            return Image.open(filename)
+
+    def __getitem__(self, idx):
+        # mask_file = list(self.masks_dir.glob(name + self.mask_suffix + '.*'))
+        # img_file = list(self.images_dir.glob(name + '.*'))
+        #
+        # assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
+        # assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
+        # mask = self.load(mask_file[0])
+        # img = self.load(img_file[0])
+        mask = self.load(self.masks[idx])
+        img = self.load(self.images[idx])
+
+        img = img.resize((256, 256))
+        assert img.size == mask.size, \
+            f'Image and mask {self.masks[idx]} should be the same size, but are {img.size} and {mask.size}'
+
+        img = self.preprocess(img, self.scale, is_mask=False)
+        mask = self.preprocess(mask, self.scale, is_mask=True)
+
+        return {
+            'image': torch.as_tensor(img.copy()).float().contiguous(),
+            'mask': torch.as_tensor(mask.copy()).long().contiguous()
+        }
+
+if __name__ == "__main__":
+
+    images_dir = '/media/D_4TB/YL_4TB/Segmentation/data/DLRSD/Images'
+    masks_dir = '/media/D_4TB/YL_4TB/Segmentation/data/DLRSD/masks'
+    # images_dir = Path(images_dir)
+    # files = images_dir.glob('**/*.tif')
+    # files = images_dir.glob('**/*.tif')
+    datasets = DLRSDDatasets(images_dir, masks_dir)
